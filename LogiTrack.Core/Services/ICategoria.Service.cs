f9ccb40a7974d4c855ccd9cec00790762480334e
EDUARDO LACERDA SILVA <eduardo.silva1320@etec.sp.gov.br>
2025-06-12 20:53:04 -0300

    feat(core): implement services interfaces

---
+++ LogiTrack.Core/Services/ICategoria.Service.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LogiTrack.Core.Models;
+
+namespace LogiTrack.Core.Services
+{
+    public interface ICategoriaService
+    {
+        Task<IEnumerable<Categoria>> ObterTodasAsync();
+        Task<Categoria?> ObterPorIdAsync(int id);
+        Task<Categoria> AdicionarAsync(Categoria categoria);
+        Task<Categoria> AtualizarAsync(Categoria categoria);
+        Task<bool> ExcluirAsync(int id);
+        Task<IEnumerable<Categoria>> ObterAtivasAsync();
+    }
+}
